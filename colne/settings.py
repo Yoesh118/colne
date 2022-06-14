dab301dee29549e5c0dcdf6db3a821c2012eab5b
Yoesh118 <69466066+Yoesh118@users.noreply.github.com>
2022-06-14 20:37:24 +0200

    Update settings.py

--- colne/settings.py
+++ colne/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bd0u!rj!ve7*h4hbq4r9snx5-gsi^@-#amrn0q_0+!*_m*9m2y'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
